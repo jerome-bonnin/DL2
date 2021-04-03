317f46264d5edabfa2d6bc47ccdbd5c4431e218a
Jérôme Bonnin <bonnin.j49@gmail.com>
2021-04-03 22:27:09 +0200

    fonction lire_alpha_digit complete

--- code/principal_RBM_alpha.py
+++ code/principal_RBM_alpha.py
@@ -1,5 +1,20 @@
-def lire_alpha_digit():
-    return 0
+import numpy as np
+from scipy.io import loadmat
+
+def lire_alpha_digit(caracteres):
+    idx = list(caracteres)
+    for i, a in enumerate(caracteres):
+        try:
+            idx[i] = int(a)
+        except ValueError:
+            idx[i] = ord(a) - ord('a') + 10
+    bad = loadmat('../data/binaryalphadigs.mat')
+    data = list()
+    for j in idx:
+        sample = [np.expand_dims(img.flatten(), axis=0) for img in bad['dat'][j,:]]
+        data.extend(np.concatenate(sample, axis=0))
+    data = np.array(data)
+    return data
 
 def init_RBM():
     return 0

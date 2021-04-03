4f9ca1880f7fd1d7a92e137e6931dbe8a33b5cb0
Louis <lmercier36@gmail.com>
2021-04-03 18:08:30 +0200

    Ajout du DBN

--- code/principal_DBN_alpha.py
+++ code/principal_DBN_alpha.py
@@ -1,8 +1,24 @@
-def init_DNN():
-    return 0
+from principal_RBM_alpha import RBM
 
-def pretrain_DNN():
-    return 0
+def class DNN:
+    def __init__(nb_couches, p, q):
+        self.DNN_list = []
+        self.nb_couches = nb_couches
+        DNN_list.append(RBM(p, q))
+        for i in range(nb_couches):
+            DNN_list.append(RBM(q, q))
+            '''
+            La premiere couche a la même forme que le RBM classique.
+            Les autres couches seront des couches carrées par souci de compatibilité et de simplicité.
+            p (resp. q) représente la taille des données d'entrée (resp. la taille de la sortie)
+            '''
 
-def generer_image_DBN():
-    return 0
+    def pretrain_DNN(self, nb_iter, learning_rate, batch_size, x):
+        for i in range (self.nb_couches):
+            print("#########################")
+            print("RBM numéro " + i)
+            L[i].train(nb_iter, learning_rate, batch_size, x)
+            x = L[i].entree_sortie(x)
+
+    def generer_image_DBN():
+        return 0

3578c01953f7a9baa537e0682e08c702a9101d6f
Louis <lmercier36@gmail.com>
2021-04-04 12:02:45 +0200

    Ajout du calcul de l'erreur

--- code/principal_DNN_MNIST.py
+++ code/principal_DNN_MNIST.py
@@ -1,5 +1,6 @@
 from principal_RBM_alpha import RBM
 from principal_DBN_alpha import DNN
+from utils import cross_entropy
 
 def calcul_softmax(layer, x):
     z = layer.entree_sortie(x)
@@ -16,7 +17,7 @@ def entree_sortie_reseau(network, x):
     list_res.append(z)
     return(list_res)
 
-def retropropagation(network, num_iter, learning_rate, batch_size, data, data_label):
+def retropropagation(network, num_iter, learning_rate, batch_size, data, data_label): #Attention aux dimensions
     eps = learning_rate
     nb_elm = len(data)//batch_size
     data = np.random.shuffle(data)
@@ -29,33 +30,36 @@ def retropropagation(network, num_iter, learning_rate, batch_size, data, data_la
             #L'ensemble des x(l)
             list_x = entree_sortie_reseau(network, data_batch)
             #Cas de fin de réseau
-            c = [list_x[len(list_x) -1] - data_label]
-            grad_W = np.dot(c,list_x[len(list_x) -2])
+            x = list_x[len(list_x) -1] #représente x^(p)
+            x_moins = list_x[len(list_x) -2] #Représente x^(p-1)
+            c = [x - data_label]
+            grad_W = np.dot(c,x_moins)
             grad_b = c
-            c_list[len(list_x) -1] = c
-            list_RBM[len(list_x) -1].W = W + eps*grad_W
-            list_RBM[len(list_x) -1].b = b + eps*grad_b
+            c_plus= c #Représente c^(p+1)
+            list_RBM[-1].W = W + eps*grad_W #Modification des poids du réseau
+            list_RBM[-1].b = b + eps*grad_b #Modification des biais du réseau
             #Cas général
-            for k in range(len(list_x) - 2, -1, -1):
+            for k in range(len(list_x) - 2, 0, -1): #Pas sûr de l'indexation
                 x = list_x[k]
-                c = np.dot(c_list[k+1], list_RBM[k+1])*.x*.(1 -. x)
-                c_list[k] = c
-                grad_W = np.dot(c,list_x[k])
-                grad_b = c
-                list_RBM[k].W = W + eps*grad_W
-                list_RBM[k].b = b + eps*grad_b
-            network.list_RBM = list_RBM
+                x_moins = list_x[k-1]
+                c = np.dot(c_plus, list_RBM[k+1])*.x*.(1 -. x)
+                c_plus = c
+                grad_W = np.dot(c,x_moins) / len(x)
+                grad_b = np.mean(c, axis=0, keepdims=True)
+                list_RBM[k].W = W + eps*grad_W #Modification des poids du réseau
+                list_RBM[k].b = b + eps*grad_b #Modification des biais du réseau
+            network.list_RBM = list_RBM #Modification de l'objet réseau
             y_hat = entree_sortie_reseau(network, data_batch)
-            loss = np.dot(data_label_batch, np.transpose(y_hat))
+            loss = cross_entropy(y_hat, data_batch_label)
             '''
             Faux car, les labels n'ont pas été ajoutés au batch
-            Faux aussi car ceci est la formule de la binary cross entropy, il faut binariser les labels.
-            Il faudrait binariser les valeurs de data_label.
-            Ici, y_hat est une proba, il ne changerait pas.
             '''
         print("----------------------------------------")
         print("EPOCH = " + i)
         print("Error = " + loss)
 
-def test_DNN():
-    return 0
+def test_DNN(network, data_test, data_label):
+    sortie = network.entree_sortie_reseau(data_test)
+    y_hat = sortie[-1] #ici, y_hat est une matrice de proba pour chaque "individus"
+    error = cross_entropy(y_hat, data_label)
+    return (error)
